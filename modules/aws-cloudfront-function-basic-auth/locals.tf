80d6d048bdf229a9921fb90592636d26c9851867
Jordan Dziat <jdziat@ltvco.com>
2021-05-31 12:05:45 -0400

    function name updates

--- modules/aws-cloudfront-function-basic-auth/locals.tf
+++ modules/aws-cloudfront-function-basic-auth/locals.tf
@@ -1,6 +1,6 @@
 locals {
   tags          = merge(var.tags, var.default_tags)
-  function_name = var.function_name != "" ? var.function_name : "basic-auth-${formatdate("YYYYMMDD", timestamp())}"
+  function_name = var.function_name != "" ? var.function_name : "basic-auth-${formatdate("YYYYMMDD", time_static.this.rfc3339)}"
   username      = var.username
   password      = var.password != "" ? var.password : random_password.this.result
 }
