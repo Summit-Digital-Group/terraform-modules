5cd8fbe5e77d0974141b4db68440db0e033a5717
Jordan Dziat <jdziat@ltvco.com>
2021-06-13 12:33:28 -0400

    Adding more criteria to cache-control

--- modules/aws-cloudfront-response-cache-header/function.js
+++ modules/aws-cloudfront-response-cache-header/function.js
@@ -2,8 +2,9 @@ function handler(event) {
   var response = event.response;
   var request = event.request;
   var headers = response.headers;
+  var regex = /\.(css|svg|png|jpeg|jpg|gif|pdf|svg|md|ico|js|htm|html|txt|md|woff2|woff|webp|ttf|xml|doc|docx|bmp|mp3|mp4)$/i
 
-  if(request.uri !=="/index.html") {
+  if(regex.test(request.uri)&&request.uri!=="/index.html") {
     // Set the cache-control header
     headers['cache-control'] = { value: 'public, max-age=${duration};' };
   }
